247c8fbcfb9a377e34930c26d9dfa673c8dce50d
joakimran5 <joakimran5@gmail.com>
2026-06-11 14:18:53 +0800

    Create init_database

---
+++ scripts/init_database.sql
@@ -0,0 +1,14 @@
+/* Create DB and Schema */
+
+use master;
+
+CREATE DATABASE Datawarehouse_crmSales;
+
+USE Datawarehouse_crmSales;
+
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+
